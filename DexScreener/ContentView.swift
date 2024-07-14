4fd85f80545a4f3ce44c3207ed6966b78c4de0b5
Waqas R <waqasraza123@gmail.com>
2024-07-14 09:59:46 +0500

    Initial Commit

---
+++ DexScreener/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DexScreener
+//
+//  Created by Waqas on 7/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DexScreener/DexScreenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DexScreenerApp.swift
+//  DexScreener
+//
+//  Created by Waqas on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DexScreenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
