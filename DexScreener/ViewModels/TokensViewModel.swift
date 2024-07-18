89519450e0982bdaeeedc99559fd96285577b6d1
Waqas R <waqasraza123@gmail.com>
2024-07-18 14:52:12 +0500

    show loading icon when data is being fetched

--- DexScreener/ViewModels/TokensViewModel.swift
+++ DexScreener/ViewModels/TokensViewModel.swift
@@ -1,30 +1,41 @@
-import Foundation
+import SwiftUI
 
 class TokensViewModel: ObservableObject {
     @Published var tokens: [Token] = []
+    @Published var isLoading: Bool = false // Track loading state
 
     init() {
         fetchTokens()
     }
 
     func fetchTokens() {
+        isLoading = true // Start loading
         guard let url = URL(string: Constants.solanaApiUrl) else {
             print("Invalid URL")
+            isLoading = false // Stop loading if URL is invalid
             return
         }
 
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let data = data {
                 do {
+                    print(data)
                     let decodedResponse = try JSONDecoder().decode(TokenResponse.self, from: data)
                     DispatchQueue.main.async {
                         self.tokens = decodedResponse.pairs
+                        self.isLoading = false // Stop loading when data is fetched
                     }
                 } catch {
                     print("Failed to decode JSON: \(error)")
+                    DispatchQueue.main.async {
+                        self.isLoading = false // Stop loading if there's an error
+                    }
                 }
             } else if let error = error {
                 print("Failed to fetch data: \(error)")
+                DispatchQueue.main.async {
+                    self.isLoading = false // Stop loading if there's an error
+                }
             }
         }.resume()
     }
--- DexScreener/Views/TokensView.swift
+++ DexScreener/Views/TokensView.swift
@@ -9,49 +9,56 @@ struct TokensView: View {
 
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack(alignment: .leading, spacing: 16) {
-                    ForEach(viewModel.tokens) { token in
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Text("\(token.baseToken.symbol) / \(token.quoteToken.symbol)")
-                                    .font(.headline)
-                                    .foregroundColor(.primary)
-                                Spacer()
-                                Text("Price: \(token.priceUsd)")
-                                    .font(.subheadline)
-                                    .foregroundColor(.secondary)
-                            }
-                            Divider()
-                            HStack {
+            Group {
+                if viewModel.isLoading {
+                    ProgressView("Loading...") // Show loading indicator
+                        .progressViewStyle(CircularProgressViewStyle())
+                } else {
+                    ScrollView {
+                        VStack(alignment: .leading, spacing: 16) {
+                            ForEach(viewModel.tokens) { token in
                                 VStack(alignment: .leading) {
-                                    Text("24h Volume")
-                                        .font(.caption)
-                                        .foregroundColor(.secondary)
-                                    Text("\(token.volume.h24, specifier: "%.2f")")
-                                        .font(.body)
-                                }
-                                Spacer()
-                                VStack(alignment: .leading) {
-                                    Text("Transactions (Last 24h)")
-                                        .font(.caption)
-                                        .foregroundColor(.secondary)
                                     HStack {
-                                        Text("Buys: \(token.txns.h24.buys)")
-                                            .font(.body)
-                                        Text("Sells: \(token.txns.h24.sells)")
-                                            .font(.body)
+                                        Text("\(token.baseToken.symbol) / \(token.quoteToken.symbol)")
+                                            .font(.headline)
+                                            .foregroundColor(.primary)
+                                        Spacer()
+                                        Text("Price: \(token.priceUsd)")
+                                            .font(.subheadline)
+                                            .foregroundColor(.secondary)
+                                    }
+                                    Divider()
+                                    HStack {
+                                        VStack(alignment: .leading) {
+                                            Text("24h Volume")
+                                                .font(.caption)
+                                                .foregroundColor(.secondary)
+                                            Text("\(token.volume.h24, specifier: "%.2f")")
+                                                .font(.body)
+                                        }
+                                        Spacer()
+                                        VStack(alignment: .leading) {
+                                            Text("Transactions (Last 24h)")
+                                                .font(.caption)
+                                                .foregroundColor(.secondary)
+                                            HStack {
+                                                Text("Buys: \(token.txns.h24.buys)")
+                                                    .font(.body)
+                                                Text("Sells: \(token.txns.h24.sells)")
+                                                    .font(.body)
+                                            }
+                                        }
                                     }
                                 }
+                                .padding()
+                                .background(Color(UIColor.secondarySystemBackground))
+                                .cornerRadius(10)
+                                .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
                             }
                         }
                         .padding()
-                        .background(Color(UIColor.secondarySystemBackground))
-                        .cornerRadius(10)
-                        .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
                     }
                 }
-                .padding()
             }
             .navigationBarTitle("Tokens")
             .onAppear {
