99a461cb5daba71817c2ce5d4416b2bfd0ae629a
Waqas Raza <waqasraza123@gmail.com>
2024-08-06 22:43:00 +0500

    updated navigation stack

--- DexScreener/Views/Top100CryptocurrenciesView.swift
+++ DexScreener/Views/Top100CryptocurrenciesView.swift
@@ -2,68 +2,60 @@ import SwiftUI
 
 struct Top100CryptocurrenciesView: View {
     @StateObject private var viewModel = GeckoViewModel()
-    @State private var selectedCrypto: GeckoToken?
-    
+
     var body: some View {
-        NavigationStack {
-            Group {
-                if viewModel.isLoading {
-                    ProgressView("Loading...") // Show loading indicator
-                        .progressViewStyle(CircularProgressViewStyle())
-                } else if let errorMessage = viewModel.errorMessage {
-                    Text("Error: \(errorMessage)")
-                        .foregroundColor(.red)
-                } else {
-                    List(viewModel.cryptocurrencies) { crypto in
-                        NavigationLink(value: crypto) {
-                            HStack {
-                                HStack {
-                                    AsyncImage(url: URL(string: crypto.image)) { phase in
-                                        switch phase {
-                                        case .success(let image):
-                                            image
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: 30, height: 30)
-                                                .clipShape(Circle())
-                                        case .failure(_):
-                                            Image(systemName: "photo") // Fallback image
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: 30, height: 30)
-                                                .clipShape(Circle())
-                                        case .empty:
-                                            ProgressView()
-                                                .frame(width: 30, height: 30)
-                                                .clipShape(Circle())
-                                        @unknown default:
-                                            EmptyView()
-                                        }
-                                    }
-                                    
-                                    Text(crypto.symbol.uppercased())
+        Group {
+            if viewModel.isLoading {
+                ProgressView("Loading...")
+                    .progressViewStyle(CircularProgressViewStyle())
+            } else if let errorMessage = viewModel.errorMessage {
+                Text("Error: \(errorMessage)")
+                    .foregroundColor(.red)
+            } else {
+                List(viewModel.cryptocurrencies) { crypto in
+                    NavigationLink(value: crypto) {
+                        HStack {
+                            AsyncImage(url: URL(string: crypto.image)) { phase in
+                                switch phase {
+                                case .success(let image):
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 30, height: 30)
+                                        .clipShape(Circle())
+                                case .failure(_):
+                                    Image(systemName: "photo")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 30, height: 30)
+                                        .clipShape(Circle())
+                                case .empty:
+                                    ProgressView()
+                                        .frame(width: 30, height: 30)
+                                        .clipShape(Circle())
+                                @unknown default:
+                                    EmptyView()
                                 }
-                                .frame(width: 100, alignment: .leading)
-                                
-                                Text("$\(crypto.current_price, specifier: "%.2f")")
-                                    .frame(width: 90, alignment: .trailing)
-                                
-                                Text(formattedMarketCap(crypto.market_cap))
-                                    .frame(width: 110, alignment: .trailing)
                             }
-                            .padding(.vertical, 5)
+                            
+                            Text(crypto.symbol.uppercased())
+                                .frame(width: 100, alignment: .leading)
+                            
+                            Text("$\(crypto.current_price, specifier: "%.2f")")
+                                .frame(width: 90, alignment: .trailing)
+                            
+                            Text(formattedMarketCap(crypto.market_cap))
+                                .frame(width: 110, alignment: .trailing)
                         }
-                        .tag(crypto) // Use tag for navigation to match the selectedCrypto
+                        .padding(.vertical, 5)
                     }
-                    .navigationTitle("Top 100+ Tokens")
+                    .tag(crypto)
                 }
+                .navigationTitle("Top 100+ Tokens")
             }
-            .onAppear {
-                viewModel.fetchCryptocurrencies()
-            }
-            .navigationDestination(for: GeckoToken.self) { crypto in
-                OHLCChartView(tokenId: crypto.id)
-            }
+        }
+        .onAppear {
+            viewModel.fetchCryptocurrencies()
         }
     }
 }
