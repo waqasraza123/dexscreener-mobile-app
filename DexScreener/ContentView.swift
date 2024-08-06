4428cf3ff45b5d77543c39a67a685de02532fea2
Waqas Raza <waqasraza123@gmail.com>
2024-08-06 10:11:01 +0500

    updated main view

--- DexScreener/ContentView.swift
+++ DexScreener/ContentView.swift
@@ -1,8 +1,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var username: String = ""
-    @State private var password: String = ""
     @State private var isLoggedIn: Bool = false
 
     var body: some View {
@@ -11,53 +9,15 @@ struct ContentView: View {
                 if isLoggedIn {
                     HomeView()
                 } else {
-                    loginView
+                    LoginView(isLoggedIn: $isLoggedIn)
                 }
             }
         }
     }
+}
 
-    var loginView: some View {
-        VStack {
-            // Logo Image
-            Image(systemName: "star.fill") // Replace with your logo image
-                .resizable()
-                .scaledToFit()
-                .frame(width: 100, height: 100)
-                .padding()
-
-            TextField("Username", text: $username)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-
-            Button(action: {
-                // Simulate login
-                if username.lowercased() == "admin" && password == "admin" {
-                    isLoggedIn = true
-                } else {
-                    // Handle invalid credentials
-                    print("Invalid credentials")
-                }
-            }) {
-                Text("Login")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: 220, height: 60)
-                    .background(Color.blue)
-                    .cornerRadius(15.0)
-            }
-            
-            NavigationLink(destination: SignUpView()) {
-                Text("Sign Up")
-                    .foregroundColor(.blue)
-                    .padding()
-            }
-        }
-        .padding()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
